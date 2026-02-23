b5e3ff976274eeec5b9d160326692ee8fd43e7c4
maho <ohloapplicationsoft@gmail.com>
2026-02-22 19:35:41 -0800

    Initial Commit

---
+++ Kazumon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kazumon
+//
+//  Created by 0000 on 2026/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kazumon/KazumonApp.swift
@@ -0,0 +1,17 @@
+//
+//  KazumonApp.swift
+//  Kazumon
+//
+//  Created by 0000 on 2026/02/22.
+//
+
+import SwiftUI
+
+@main
+struct KazumonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
